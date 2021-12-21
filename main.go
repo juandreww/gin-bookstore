d36f5ffe0576a74e9211be5607ed4cec1dced88e
Juan Andrew <juandreww108@gmail.com>
2021-12-21 07:02:57 +0700

    put delete

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func main() {
   models.ConnectDatabase()
   r.GET("/books", controllers.FindBooks)
   r.POST("/books", controllers.CreateBook)
+  r.GET("/books/:id", controllers.FindBook)
+  r.PATCH("/books/:id", controllers.UpdateBook)
+  r.DELETE("/books/:id", controllers.DeleteBook)
 
   r.Run()
 }
